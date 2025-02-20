3efe9fd805efa60d08380d1e943866c2b8e187bc
Noah Netz <noahjnetz@gmail.com>
2025-02-20 13:34:35 -0700

    Initial planning for RGB

--- app/main.c
+++ app/main.c
@@ -1,90 +1,27 @@
-#include <msp430.h> 
-int counter = 0;
+#include "msp430fr2355.h"
+#include <msp430.h>
+#include <stdint.h>
+#include <stdbool.h>
+#define Max_PWM = 1000 // max brightness
 
-
-/**
- * main.c
- */
 int main(void)
 {
-	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
-	
-	//-- Setup Ports
-	P1DIR |= BIT0;
-	P1OUT |= BIT0;
-    // Button S1 & S2 pins as input with pull-up resistors
-    P4DIR &= ~BIT1;      // S1 P4.1
-    P4REN |= BIT1;
-    P4OUT |= BIT1;
-    P4IES |= BIT1;
+    // Stop watchdog timer
+    WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
 
-    P2DIR &= ~BIT3;      // S2 P2.3
-    P2REN |= BIT3;
-    P2OUT |= BIT3;
-    P2IES |= BIT3;
-
+    //-- Setup Ports
+    P6DIR |= (BIT0 | BIT1 | BIT2);
+    P6SEL |= (BIT0 | BIT1 | BIT2);
+    
+    // Disable the GPIO power-on default high-impedance mdoe to activate
+    // previously configure port settings
     PM5CTL0 &= ~LOCKLPM5;
 
-    //-- Setup IRQ
-    P4IFG &= ~BIT1; // Clear interrupt flag
-    P4IE |= BIT1;   // Enable interrupt for S1 (P4.1)
-
-    P2IFG &= ~BIT3; // Clear interrupt flag
-    P2IE |= BIT3;   // Enable interrupt for S2 (P2.3)
+    //-- Configure Timers
 
 
-    //-- Setup Timer
-    TB0CTL |= TBCLR;    // Clear timer and dividers
-    TB0CTL |= TBSSEL__SMCLK;    // Source = SMCLK
-    TB0CTL |= MC__UP;    // Mode = UP
-    TB0CCR0 = 1000;
-    TB0CCR1 = 250;
 
-    //-- Setup Timer Overflow IRQ
-    TB0CCTL0 &= ~CCIFG;       // Clear CCR0 Flag
-    TB0CCTL0 |= CCIE;        // Enable TB0 CCR0 Overflow IRQ
-    TB0CCTL1 &= ~CCIFG;      // Clear CCR1 Flag
-    TB0CCTL1 |= CCIE;        // Enable TB0 CCR1 Overflow IRQ
-
-    __enable_interrupt();   // enable maskable IRQs
-
-    //-- Main loop
-    while(1){}
-
-	return 0;
-}
-
-//-- Interrupt Service Routines
-#pragma vector = TIMER0_B0_VECTOR
-__interrupt void ISR_TB0_CCR0(void)
-{
-    P1OUT |= BIT0;  // toggle LED1 = 1
-    TB0CCTL0 &= ~CCIFG;   // clear CCR0 flag
-}
-#pragma vector = TIMER0_B1_VECTOR
-__interrupt void ISR_TB0_CCR1(void)
-{
-    P1OUT &= ~BIT0;  // toggle LED1 = 0
-    TB0CCTL1 &= ~CCIFG;   // clear CCR1 flag
-}
-#pragma vector = PORT4_VECTOR
-__interrupt void ISR_Port4_S1(void)
-{
-    TB0CCR1 += 25;  // Increase duty cycle by 2.5%
-    if (TB0CCR1 >= 501)  // To ensure the max limit is 500 (50%)
+    while(true)
     {
-        TB0CCR1 = 500;
     }
-    P4IFG &= ~BIT1;    // Clear interrupt flag
-}
-
-#pragma vector = PORT2_VECTOR
-__interrupt void ISR_Port2_S2(void)
-{
-    TB0CCR1 -= 25;  // Decrease duty cycle by 2.5%
-    if (TB0CCR1 <= 25)
-    {
-        TB0CCR1 = 25;
-    }
-    P2IFG &= ~BIT3;    // Clear interrupt flag
 }
